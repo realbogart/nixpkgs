9e5b78b9ac675fe85773fc7752319123e6387724
Johan Yngman <johan.yngman@gmail.com>
2025-06-06 08:32:46 +0200

    Update claude-code

--- pkgs/by-name/cl/claude-code/package.nix
+++ pkgs/by-name/cl/claude-code/package.nix
@@ -1,18 +1,14 @@
-{
-  lib,
-  buildNpmPackage,
-  fetchzip,
-  nodejs_20,
-}:
+{ lib, buildNpmPackage, fetchzip, nodejs_20, }:
 
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "1.0.11";
+  version = "1.0.16";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
-    url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
+    url =
+      "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
     hash = "sha256-IXNBNjt4Sh5pR+Cz2uEZcCop9reAmQ7hObohtN0f3Ww=";
   };
 
@@ -36,7 +32,8 @@ buildNpmPackage rec {
   passthru.updateScript = ./update.sh;
 
   meta = {
-    description = "Agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster";
+    description =
+      "Agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster";
     homepage = "https://github.com/anthropics/claude-code";
     downloadPage = "https://www.npmjs.com/package/@anthropic-ai/claude-code";
     license = lib.licenses.unfree;
